565dd5ab574f0c8b75bc87eaaadf3712365dfe5b
Leilani <pquaby@gmail.com>
2024-07-24 18:15:21 -0400

    Add Login form fields

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Signup from './components/Signup';
+
+//Components
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       < Signup />
+      < Login />
     </div>
   );
 }
