3e0e16c3693e73028632f9e64bdb52925e64865f
Leilani <pquaby@gmail.com>
2024-07-24 18:58:12 -0400

    Add submit functionality

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,36 +3,50 @@ import React, { useState } from 'react';
 
 const Login = () => {
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  //const [login, setLogin] = useState(false);
+  const [id, setid] = useState("");
+  const [login, setLogin] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(email, id);
+    setLogin(true);
+    setEmail('');
+    setid('');
+  };
 
   return (
     <div>
+
       <h1>Dicer</h1>
-      <form>
-        <div className=''>
-          <input
-            type='text'
-            placeholder='email'
-            value={email}
-            onChange={ (e) => setEmail(e.target.value)}
-            required />
-        </div>
-        <div className=''>
-          <input
-            type='text'
-            placeholder='password'
-            value={password}
-            onChange={ (e) => setPassword(e.target.value)}
-            required />
-        </div>
 
-        <button>Login</button>
+      {login ? <h2>Welcome!</h2> :
+        (
+          <form onSubmit={handleSubmit}>
+            <div className=''>
+              <input
+                type='text'
+                placeholder='email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required />
+            </div>
+            <div className=''>
+              <input
+                type='text'
+                placeholder='password'
+                value={id}
+                onChange={(e) => setid(e.target.value)}
+                required />
+            </div>
 
-        <div className=''>
-          <p>Don't have an account? <a href='#'>Sign Up!</a></p>
-        </div>
-      </form>
+            <button>Login</button>
+
+            <div className=''>
+              <p>Don't have an account? <a href='#'>Sign Up!</a></p>
+            </div>
+          </form>
+        )}
+
     </div>
   );
 };
