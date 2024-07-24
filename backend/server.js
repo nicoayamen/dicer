fda0f877072a622403ed57eb30084f833db50db1
Leilani Graham <148240283+oatmilkies@users.noreply.github.com>
2024-07-24 17:01:43 -0400

    Update server.js
    Add get route

--- backend/server.js
+++ backend/server.js
@@ -41,6 +41,9 @@ app.use('/users', usersRoutes);
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
