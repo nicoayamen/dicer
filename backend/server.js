2938ab468b7323b4f9dae3df01b140593c3f5766
Nicolas Ayala <99232598+nicoayamen@users.noreply.github.com>
2024-07-24 12:00:03 -0600

    remove get route

--- backend/server.js
+++ backend/server.js
@@ -42,10 +42,6 @@ app.use('/users', usersRoutes);
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
