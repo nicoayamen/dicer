71ef7dafc6b60ca5ef9f9439ae4a5331b7c8ef4a
Kim O'Dell <kimbo.odell@gmail.com>
2024-07-24 22:00:13 -0400

    Add user sign up form

---
+++ frontend/src/components/Signup.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+
+const Signup = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+  });
+
+  //update values as user types
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("form submitted!", formData)
+    console.log("first name", formData.firstName)
+    console.log("last name", formData.lastName)
+    console.log("email", formData.email)
+    console.log("password", formData.password)
+
+    // fetch('http://localhost:8080/signup', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify(formData)
+    // })
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     console.log('User signed up successfully:', data);
+    //     // Handle successful signup (redirect to user profile)
+    //   })
+    //   .catch(error => {
+    //     console.error('There was an error signing up:', error);
+    //     // Handle error 
+    //   });
+    
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="">
+          <label>First Name:</label>
+          <input 
+            type="text" 
+            name="firstName" 
+            value={formData.firstName} 
+            onChange={handleChange} 
+            required 
+          />
+        </div>
+        <div className="">
+          <label>Last Name:</label>
+          <input 
+            type="text" 
+            name="lastName" 
+            value={formData.lastName} 
+            onChange={handleChange} 
+            required 
+          />
+        </div>
+        <div className="">
+          <label>Email:</label>
+          <input 
+            type="email" 
+            name="email" 
+            value={formData.email} 
+            onChange={handleChange} 
+            required 
+          />
+        </div>
+        <div className="">
+          <label>Password:</label>
+          <input 
+            type="password" 
+            name="password" 
+            value={formData.password} 
+            onChange={handleChange} 
+            required 
+          />
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
