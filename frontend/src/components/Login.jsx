f97403159018dffec7cd6141101709c28814d299
Leilani <pquaby@gmail.com>
2024-07-25 17:16:36 -0400

    Add icons and password visibility toggle

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,11 +1,26 @@
 import React, { useState } from 'react';
-
+import IconEye from './IconEye';
+import IconEyeSlash from './IconEyeSlash';
+import IconBxsUser from './IconBxsUser';
+import IconBxsLockAlt from './IconBxsLockAlt';
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [id, setid] = useState("");
   const [login, setLogin] = useState(false);
   //const [error, setError] = useState("");
+  const [type, setType] = useState('password');
+  const [icon, setIcon] = useState(<IconEyeSlash />);
+
+  const handleToggle = () => {
+    if (type === 'password') {
+      setType('text');
+      setIcon(<IconEye />);
+    } else {
+      setType('password');
+      setIcon(<IconEyeSlash />);
+    }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -16,7 +31,7 @@ const Login = () => {
       console.log(body);
       const response = await fetch('/login', {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(body)
       });
 
@@ -30,7 +45,7 @@ const Login = () => {
       setLogin(true);
       setEmail('');
       setid('');
-      
+
     } catch (err) {
       console.error(err.message);
     }
@@ -43,30 +58,50 @@ const Login = () => {
 
       {login ? <h2>Welcome!</h2> :
         (
-          <form onSubmit={handleSubmit}>
-            <div className=''>
-              <input
-                type='text'
-                placeholder='email'
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required />
-            </div>
-            <div className=''>
-              <input
-                type='text'
-                placeholder='password'
-                value={id}
-                onChange={(e) => setid(e.target.value)}
-                required />
+          <form onSubmit={handleSubmit} className='login-form'>
+            <div className='login-content'>
+
+              <div className='login-box'>
+                <IconBxsUser />
+                <div className='login--box-input'>
+                  <input
+                    className='login-input'
+                    type='email'
+                    placeholder=' '
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required />
+                  <label for='' className='login-label'>Email</label>
+                </div>
+              </div>
+
+              <div className='login-box'>
+                <IconBxsLockAlt />
+                <div className='login-box-input'>
+                  <input
+                    className='login-input'
+                    type={type}
+                    placeholder=' '
+                    value={id}
+                    onChange={(e) => setid(e.target.value)}
+                    required />
+                  <label for='' className='login-label'>Password</label>
+
+                  <span onClick={handleToggle}>{icon}</span>
+
+                </div>
+
+                <button>Login</button>
+
+                <div className=''>
+                  <p>Don't have an account? <a href='#'>Sign Up!</a></p>
+                </div>
+              </div>
+
             </div>
 
-            <button>Login</button>
-
-            <div className=''>
-              <p>Don't have an account? <a href='#'>Sign Up!</a></p>
-            </div>
           </form>
+
         )}
 
     </div>
