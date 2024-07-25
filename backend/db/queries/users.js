366c9c8ab739c9582784aedca91ce3dfaa2e9aca
Leilani <pquaby@gmail.com>
2024-07-24 22:22:02 -0400

    Set up login route and query

--- backend/db/queries/users.js
+++ backend/db/queries/users.js
@@ -1,10 +1,24 @@
 const db = require('../connection');
 
-const getUsers = () => {
-  return db.query('SELECT * FROM users;')
+const getLogin = (email, id) => {
+  const queryString = `SELECT email, id FROM users
+  WHERE email = $1
+  AND id = $2;
+  `;
+  const values = [email, id];
+
+  return db.query(queryString, values)
     .then(data => {
+      console.log("query", data.rows)
+      if (data.rows.length === 0) {
+        console.log("not found")
+        return null;
+      }
       return data.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 };
 
-module.exports = { getUsers };
+module.exports = { getLogin };
