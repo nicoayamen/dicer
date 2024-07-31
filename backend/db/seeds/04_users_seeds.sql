018a93705174126ac20662f1fe4ae485ef996553
charleysmithschofield <charleysmithschofield@gmail.com>
2024-07-30 21:48:37 -0700

    both seed user photos rendering on Profile and Edit Profile pages. Photo does not currently display when user clicks update on Edit Profile and is redirected to Profile.

--- backend/db/seeds/04_users_seeds.sql
+++ backend/db/seeds/04_users_seeds.sql
@@ -2,8 +2,8 @@
 
 -- Insert first user
 INSERT INTO users (first_name, last_name, photo, availability_id, role_id, email, password)
-VALUES ('Jasper', 'Smith', 'backend/db/docs/profile-images/User1-1.png', 1, 1, 'jasper_s@example.com', 'letmein');
+VALUES ('Jasper', 'Smith', 'https://raw.githubusercontent.com/nicoayamen/dicer/dev/backend/db/docs/profile-images/User1-1.png', 1, 1, 'jasper_s@example.com', 'letmein');
 
 -- Insert second user
 INSERT INTO users (first_name, last_name, photo, availability_id, role_id, email, password)
-VALUES ('Eve', 'Johnson', 'backend/db/docs/profile-images/User2-1.jpg', 1, 1, 'eve_j@example.com', 'password1234');
+VALUES ('Eve', 'Johnson', 'https://raw.githubusercontent.com/nicoayamen/dicer/dev/backend/db/docs/profile-images/User2-1.jpg', 2, 2, 'eve_j@example.com', 'password1234');
