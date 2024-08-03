a8c9f89e6d11f271d6593a80c8a4c0dd593d408c
Leilani <pquaby@gmail.com>
2024-08-03 12:35:56 -0400

    Add styling

--- frontend/src/components/MatchedModal.jsx
+++ frontend/src/components/MatchedModal.jsx
@@ -10,10 +10,13 @@ const style = {
   left: '50%',
   transform: 'translate(-50%, -50%)',
   width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: '#423e6b',
+  borderRadius: 5,
   boxShadow: 24,
   p: 4,
+  outline: 'none',
+  color: 'white',
+  textAlign: 'center'
 };
 
 export default function MatchedModal(props) {
