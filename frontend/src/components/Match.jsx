6c62728e4db1e3425bec3f9442579e3df2e3b16b
charleysmithschofield <charleysmithschofield@gmail.com>
2024-08-04 22:23:44 -0700

    Edit end of match users display message and style it. Modify db to remove character_name, modify names to be present in user bios

--- frontend/src/components/Match.jsx
+++ frontend/src/components/Match.jsx
@@ -74,7 +74,11 @@ const CardStack = () => {
           nextUser={nextUser}
         />
       ) : (
-        <p>no more players :(</p>
+        <p className="no-more-matches">
+            Sorry, there are no more adventurers or DMs to match with!
+            <br />
+            Please check back later for future matches.
+        </p>
       )}
     </div>
   );
