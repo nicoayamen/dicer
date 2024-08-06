28309e1bbfc643ad5baed1012ce1051f0a1b81f6
charleysmithschofield <charleysmithschofield@gmail.com>
2024-08-05 21:24:56 -0700

    Modify all schema files that contain user_id as FK so user can delete their profile. Update roles to remove un-used character_name

--- frontend/src/components/EditProfile.jsx
+++ frontend/src/components/EditProfile.jsx
@@ -55,11 +55,12 @@ const EditProfile = (props) => {
   }, [userId]);
 
   const handleChange = (e) => {
-    const { name, value, type } = e.target;
-    setFormData({
-      ...formData,
-      [name]: type === 'radio' ? value === 'true' : value
-    });
+    const { name, value, type, checked } = e.target;
+    setFormData(prevFormData => ({
+      ...prevFormData,
+      [name]: type === 'radio' ? checked : value,
+      classType: name === 'isDM' && checked ? null : prevFormData.classType
+    }));
   };
 
   //Handles file input changes: updates the image preview and sets the selected file in the form data.
@@ -80,7 +81,7 @@ const EditProfile = (props) => {
     data.append('firstName', formData.firstName);
     data.append('lastName', formData.lastName);
     data.append('email', formData.email);
-    data.append('classType', formData.classType);
+    data.append('classType', formData.classType || null);
     data.append('isDM', formData.isDM);
     data.append('bio', formData.bio);
     if (formData.roleId) {
@@ -195,7 +196,7 @@ const EditProfile = (props) => {
                 </InputLabel>
                 <Select
                   name='classType'
-                  value={formData.classType}
+                  value={formData.classType || ''}
                   onChange={handleChange}
                   color="secondary"
                 >
