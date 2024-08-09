46d064af5435b294a7aaab6f1dad59458fd175ad
Charley Smith-Schofield <csmithschofield@gmail.com>
2024-08-08 19:22:05 -0700

    Add dark mode styling for Login Page and Dark Mode Toggle in place of Nav Bar on Login Page

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import DarkModeToggle from './DarkModeToggle';
 import '../styles/login.css';
 
 //Components
@@ -7,9 +8,18 @@ import Login from './Login';
 
 const LoginPage = (props) => {
   const { email, setEmail, password, setPassword, error, setError, setLogin } = props;
+  const [darkMode, setDarkMode] = useState(() => localStorage.getItem('darkmode') === 'true');
+
+  // Dark mode toggle function
+  const toggleDarkMode = () => {
+    setDarkMode(prevMode => !prevMode);
+    localStorage.setItem('darkmode', !darkMode);
+    document.body.classList.toggle('darkmode', !darkMode);
+  };
 
   return (
     <div>
+      <DarkModeToggle darkMode={darkMode} toggleDarkMode={toggleDarkMode} /> 
       <Login email={email} setEmail={setEmail} password={password} setPassword={setPassword} error={error} setError={setError} setLogin={setLogin} />
 
       <div className='sign-up'>
